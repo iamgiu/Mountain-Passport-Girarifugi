0f2706e81dcc45fe45fb648629b0332da5ceabed
Giulia Righi <righi.giulia@outlook.com>
2025-06-06 22:29:43 +0200

    menu modificato

--- app/src/main/java/com/example/mountainpassport_girarifugi/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/mountainpassport_girarifugi/ui/profile/ProfileFragment.kt
@@ -1,4 +1,25 @@
 package com.example.mountainpassport_girarifugi.ui.profile
 
-class ProfileFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mountainpassport_girarifugi.R
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflata il layout del fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
